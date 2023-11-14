f2868b366cdae710ffa09f096e91592056f9ddf9
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-11-14 15:25:34 +0000

    completed app

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+
+  const{search, animals}=useAminalSearch();
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      ></input>
+      <ul>
+        {animals.map((animal) => {
+          return(
+            <Animal key={animal.id}{...animal}/>
+          )
+        })}
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
+}
+
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name}({age} years old)
+    </li>
+  );
+}
+
+
+function useAminalSearch(){
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(()=>{
+    const lastQuery=localStorage.getItem('lastQuery');
+    search(lastQuery)
+  }, []);
+
+  const search= async(q)=>{
+    const response= await fetch('http://localhost:8080?'+ new URLSearchParams({ q })
+    );
+    const data= await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q)
+  };
+  return{search, animals};
+}
+
+export default App;
